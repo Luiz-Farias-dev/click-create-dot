4377edb1a389bc42140c5fedd5449ac64ce85bd0
LuNe137 <luizferreirafariasn@gmail.com>
2023-01-05 19:23:24 -0300

    full project click dot

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [click, setClick] = useState([]);
+  const [undid, setUndid] = useState([]);
+  const handleClick = (event) => {
+    const whereCLicked = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    };
+    setClick((prev)=> [...prev, whereCLicked])
+  }
+
+  const handleUndo = () => {
+    if(click.length === 0) {
+      return;
+    }
+    const lastItem = click[click.length - 1];
+    setUndid((prev) => [...prev, lastItem]);
+    setClick((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  const handleRedo = () => {
+    if(undid.length === 0) {
+      return;
+    }
+   const lastItem = undid[undid.length - 1];
+   setUndid((prev) => {
+    const newArr = [...prev].slice(0, -1);
+    return newArr;
+  })
+   setClick((prev) => [...prev, lastItem]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <button 
+        className='btn-undo'
+        onClick={handleUndo}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Desfazer
+      </button>
+      <button 
+        className='btn-redo'
+        onClick={handleRedo}
+        >
+          Refazer
+      </button>
+      <div className="App" onClick={(e)=>handleClick(e)}>
+        {click.map((item, index) => (
+          <span key={index} className='dot' style={{left: item.clientX, top: item.clientY}}/>
+        ))}
+      </div>
+    </>
+    
   );
 }
 
